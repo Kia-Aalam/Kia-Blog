bc8a5600de0b1ff19306e05a95361a3c8a9a6e5c
Kia Aalam <kiaaalam7@gmail.com>
2026-07-05 14:27:24 +0330

    complete login page (sign in)

--- login/views.py
+++ login/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "The Username or Password is incorrect!")
+            return render(request, "login/login.html")
+        
+    return render(request, 'login/login.html')
